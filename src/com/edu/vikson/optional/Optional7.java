86ba406db56f8751da1d2c5c0a0e078dd3b6d85d
Sonkar <vikalp.sonkar@accenture.com>
2023-03-21 02:00:45 +0530

    Optional Version 1.0

---
+++ src/com/edu/vikson/optional/Optional7.java
@@ -0,0 +1,14 @@
+package com.edu.vikson.optional;
+
+import java.util.Optional;
+
+public class Optional7 {
+
+	public static void main(String[] args) {
+		
+		var optional= Optional.ofNullable(null);
+		System.out.println(optional.isPresent());
+
+	}
+
+}
